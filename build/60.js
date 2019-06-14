f322f6879cec4e0007a543461f3ac2bc290987e7
datnguyenthang <nguyen.thang.dat.nt@gmail.com>
2019-06-14 15:13:57 +0700

    fix qr transparent

--- build/60.js
+++ build/60.js
@@ -194,7 +194,7 @@ var AddonQRScannerPage = /** @class */ (function () {
     AddonQRScannerPage.prototype.ionViewWillEnter = function () {
         this.setupScanner();
         this.qrScanner.show();
-        // Hide window.document.querySelector('ion-app > .app-root').classList.add('hide');
+        window.document.querySelector('ion-app > .app-root').classList.add('transparentBody');
     };
     AddonQRScannerPage.prototype.ionViewWillLeave = function () {
         // Hide window.document.querySelector('ion-app > .app-root').classList.remove('hide');
@@ -218,20 +218,20 @@ var AddonQRScannerPage = /** @class */ (function () {
     AddonQRScannerPage.prototype.ngOnDestroy = function () { };
     __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_9" /* ViewChild */])(__WEBPACK_IMPORTED_MODULE_3__components_tabs_tabs__["a" /* CoreTabsComponent */]),
-        __metadata("design:type", __WEBPACK_IMPORTED_MODULE_3__components_tabs_tabs__["a" /* CoreTabsComponent */])
+        __metadata("design:type", typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_3__components_tabs_tabs__["a" /* CoreTabsComponent */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_3__components_tabs_tabs__["a" /* CoreTabsComponent */]) === "function" && _a || Object)
     ], AddonQRScannerPage.prototype, "tabsComponent", void 0);
     __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_9" /* ViewChild */])('searchbar'),
-        __metadata("design:type", __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["z" /* Searchbar */])
+        __metadata("design:type", typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["z" /* Searchbar */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["z" /* Searchbar */]) === "function" && _b || Object)
     ], AddonQRScannerPage.prototype, "searchbar", void 0);
     AddonQRScannerPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
             selector: 'page-addon-my-qrscanner',template:/*ion-inline-start:"/Users/vndtadmin/Desktop/Moodlemobile_lang/src/addon/qrscanner/pages/my-qrscanner/my-qrscanner.html"*/'<ion-header>\n    <ion-navbar>\n        <ion-buttons left>\n            <button ion-button (click)="closeCamera()" class="bar-button-close"><ion-icon name="close"></ion-icon></button>\n        </ion-buttons>        \n        <ion-title>{{ \'addon.qrscanner.qrscanner\' | translate }}</ion-title>\n    </ion-navbar>\n</ion-header>\n<ion-content no-scroll class="qrscanner">\n    <div class="qrscanner-area">    \n    </div>       \n    <div class="button-bottom">\n        <button (click)="toggleLight()" ion-fab class="text-uppercase">\n            <ion-icon name="flash"></ion-icon>                  \n        </button>\n        <button (click)="toggleCamera()" ion-fab class="text-uppercase">\n            <ion-icon name="reverse-camera"></ion-icon>                  \n        </button>\n    </div>    \n</ion-content>'/*ion-inline-end:"/Users/vndtadmin/Desktop/Moodlemobile_lang/src/addon/qrscanner/pages/my-qrscanner/my-qrscanner.html"*/,
         }),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["t" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["u" /* NavParams */], __WEBPACK_IMPORTED_MODULE_4__ionic_native_qr_scanner__["a" /* QRScanner */],
-            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["F" /* ViewController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["b" /* AlertController */], __WEBPACK_IMPORTED_MODULE_2__providers_data_service__["a" /* DataService */]])
+        __metadata("design:paramtypes", [typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["t" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["t" /* NavController */]) === "function" && _c || Object, typeof (_d = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["u" /* NavParams */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["u" /* NavParams */]) === "function" && _d || Object, typeof (_e = typeof __WEBPACK_IMPORTED_MODULE_4__ionic_native_qr_scanner__["a" /* QRScanner */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_4__ionic_native_qr_scanner__["a" /* QRScanner */]) === "function" && _e || Object, typeof (_f = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["F" /* ViewController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["F" /* ViewController */]) === "function" && _f || Object, typeof (_g = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["b" /* AlertController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["b" /* AlertController */]) === "function" && _g || Object, typeof (_h = typeof __WEBPACK_IMPORTED_MODULE_2__providers_data_service__["a" /* DataService */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__providers_data_service__["a" /* DataService */]) === "function" && _h || Object])
     ], AddonQRScannerPage);
     return AddonQRScannerPage;
+    var _a, _b, _c, _d, _e, _f, _g, _h;
 }());
 
 //# sourceMappingURL=my-qrscanner.js.map
