43c5e5cc3cf365c0e23fae382865020783540c72
datnguyenthang <nguyen.thang.dat.nt@gmail.com>
2019-06-20 16:44:54 +0700

    update user info on evaluate course

--- build/120.js
+++ build/120.js
@@ -1,6 +1,6 @@
 webpackJsonp([120],{
 
-/***/ 1854:
+/***/ 1857:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -13,7 +13,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__components_components_module__ = __webpack_require__(13);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__directives_directives_module__ = __webpack_require__(14);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pipes_pipes_module__ = __webpack_require__(62);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__input_info__ = __webpack_require__(1994);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__input_info__ = __webpack_require__(1999);
 // (C) Copyright 2015 Martin Dougiamas
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -66,7 +66,7 @@ var AddonEvaluateInputInfoPagePageModule = /** @class */ (function () {
 
 /***/ }),
 
-/***/ 1994:
+/***/ 1999:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -78,15 +78,16 @@ var AddonEvaluateInputInfoPagePageModule = /** @class */ (function () {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__providers_utils_utils__ = __webpack_require__(5);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__providers_sites__ = __webpack_require__(2);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__providers_events__ = __webpack_require__(12);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__providers_data_service__ = __webpack_require__(945);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__providers_app__ = __webpack_require__(9);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__addon_block_timeline_providers_timeline__ = __webpack_require__(200);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__core_course_providers_helper__ = __webpack_require__(23);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_11__core_course_providers_options_delegate__ = __webpack_require__(82);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_12__components_tabs_tabs__ = __webpack_require__(138);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_13__angular_forms__ = __webpack_require__(24);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_14__providers_evaluate__ = __webpack_require__(396);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_15__ngx_translate_core__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__core_user_providers_user__ = __webpack_require__(25);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__providers_data_service__ = __webpack_require__(945);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__providers_app__ = __webpack_require__(9);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__addon_block_timeline_providers_timeline__ = __webpack_require__(200);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_11__core_course_providers_helper__ = __webpack_require__(23);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_12__core_course_providers_options_delegate__ = __webpack_require__(82);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_13__components_tabs_tabs__ = __webpack_require__(138);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_14__angular_forms__ = __webpack_require__(24);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_15__providers_evaluate__ = __webpack_require__(396);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_16__ngx_translate_core__ = __webpack_require__(1);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -125,11 +126,13 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 
+
 /**
  * Page that displays the list of calendar events.
  */
 var AddonEvaluateInputInfoPage = /** @class */ (function () {
-    function AddonEvaluateInputInfoPage(domUtils, coursesProvider, utils, myOverviewProvider, sitesProvider, courseHelper, courseOptionsDelegate, eventsProvider, navCtrl, appProvider, evaluateProvider, fb, translate, alertController, navParams, data) {
+    function AddonEvaluateInputInfoPage(domUtils, coursesProvider, utils, myOverviewProvider, sitesProvider, courseHelper, courseOptionsDelegate, eventsProvider, navCtrl, appProvider, evaluateProvider, fb, translate, alertController, navParams, data, userProvider) {
+        var _this = this;
         this.domUtils = domUtils;
         this.coursesProvider = coursesProvider;
         this.utils = utils;
@@ -142,14 +145,19 @@ var AddonEvaluateInputInfoPage = /** @class */ (function () {
         this.translate = translate;
         this.alertController = alertController;
         this.data = data;
+        this.userProvider = userProvider;
         this.pageLoaded = false;
         this.boxClass = '';
         this.prefetchIconsInitialized = false;
         this.courseIds = '';
         this.evaluateProvider = evaluateProvider;
         this.loadSiteInfo();
+        this.userProvider.getProfile(this.sitesProvider.getCurrentSiteUserId(), 1).then(function (user) {
+            _this.credForm.controls['fullname'].setValue(user.fullname);
+            _this.credForm.controls['email'].setValue(user.email);
+        });
         this.credForm = fb.group({
-            coursecode: ['', __WEBPACK_IMPORTED_MODULE_13__angular_forms__["h" /* Validators */].required],
+            coursecode: ['', __WEBPACK_IMPORTED_MODULE_14__angular_forms__["h" /* Validators */].required],
             fullname: [''],
             email: [''],
             cmnd: ['']
@@ -228,8 +236,8 @@ var AddonEvaluateInputInfoPage = /** @class */ (function () {
         this.updateSiteObserver && this.updateSiteObserver.off();
     };
     __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_9" /* ViewChild */])(__WEBPACK_IMPORTED_MODULE_12__components_tabs_tabs__["a" /* CoreTabsComponent */]),
-        __metadata("design:type", __WEBPACK_IMPORTED_MODULE_12__components_tabs_tabs__["a" /* CoreTabsComponent */])
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_9" /* ViewChild */])(__WEBPACK_IMPORTED_MODULE_13__components_tabs_tabs__["a" /* CoreTabsComponent */]),
+        __metadata("design:type", __WEBPACK_IMPORTED_MODULE_13__components_tabs_tabs__["a" /* CoreTabsComponent */])
     ], AddonEvaluateInputInfoPage.prototype, "tabsComponent", void 0);
     __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_9" /* ViewChild */])('searchbar'),
@@ -237,15 +245,16 @@ var AddonEvaluateInputInfoPage = /** @class */ (function () {
     ], AddonEvaluateInputInfoPage.prototype, "searchbar", void 0);
     AddonEvaluateInputInfoPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-addon-evaluate-input-info',template:/*ion-inline-start:"/Users/vndtadmin/Desktop/Moodlemobile_lang/src/addon/evaluate/pages/input-info/input-info.html"*/'<ion-header>\n    <ion-navbar>\n        <ion-title (click)="exitQR()"><core-format-text [text]="siteName"></core-format-text></ion-title>\n        <ion-buttons end>\n            <button *ngIf="searchEnabled" ion-button icon-only (click)="openSearch()" [attr.aria-label]="\'core.courses.searchcourses\' | translate">\n                <ion-icon name="search"></ion-icon>\n            </button>\n        </ion-buttons>\n    </ion-navbar>\n</ion-header>\n<ion-content class="core-center-view">\n    <div class="box" ion-fixed >\n        <form ion-list [formGroup]="credForm" (ngSubmit)="checkcode()">\n            <div text-wrap text-center margin-bottom>\n                <h3>{{ \'addon.evaluate.evaluate\' | translate }}</h3>\n            </div>\n            <div class="inputs-control">\n                <ion-item>\n                    <ion-icon name="code"></ion-icon>\n                    <ion-input type="text" name="coursecode" placeholder="{{ \'addon.evaluate.coursecode\' | translate }}" formControlName="coursecode" autocapitalize="none" autocorrect="off"></ion-input>\n                </ion-item>\n                <ion-item>\n                    <ion-icon name="contact"></ion-icon>\n                    <ion-input type="text" name="fullname" placeholder="{{ \'addon.evaluate.fullname\' | translate }}" formControlName="fullname" autocapitalize="none" autocorrect="off"  ></ion-input>\n                </ion-item>\n                <ion-item>\n                    <ion-icon name="mail"></ion-icon>\n                    <ion-input type="text" name="email" placeholder="{{ \'addon.evaluate.email\' | translate }}" formControlName="email" autocapitalize="none" autocorrect="off" ></ion-input>\n                </ion-item>\n                <ion-item margin-bottom>\n                    <ion-icon name="card"></ion-icon>\n                    <ion-input type="text" name="cmnd" placeholder="{{ \'addon.evaluate.cmnd\' | translate }}" formControlName="cmnd" autocapitalize="none" autocorrect="off" ></ion-input>\n                </ion-item>\n                 <button ion-button block [disabled]="!credForm.valid">{{ \'addon.evaluate.btnnext\' | translate }}</button> \n            </div>\n            <!-- <div class="thumbs-evaluate">\n                \n            </div> -->\n            <img class="thumbs-evaluate" src="assets/img/thumbs-evaluate.svg">\n        </form>\n    </div>\n</ion-content>\n'/*ion-inline-end:"/Users/vndtadmin/Desktop/Moodlemobile_lang/src/addon/evaluate/pages/input-info/input-info.html"*/,
+            selector: 'page-addon-evaluate-input-info',template:/*ion-inline-start:"/Users/vndtadmin/Desktop/Moodlemobile_lang/src/addon/evaluate/pages/input-info/input-info.html"*/'<ion-header>\n    <ion-navbar>\n        <ion-title (click)="exitQR()"><core-format-text [text]="siteName"></core-format-text></ion-title>\n        <ion-buttons end>\n            <button *ngIf="searchEnabled" ion-button icon-only (click)="openSearch()" [attr.aria-label]="\'core.courses.searchcourses\' | translate">\n                <ion-icon name="search"></ion-icon>\n            </button>\n        </ion-buttons>\n    </ion-navbar>\n</ion-header>\n<ion-content class="core-center-view">\n    <div class="box" ion-fixed >\n        <form ion-list [formGroup]="credForm" (ngSubmit)="checkcode()">\n            <div text-wrap text-center margin-bottom>\n                <h3>{{ \'addon.evaluate.evaluate\' | translate }}</h3>\n            </div>\n            <div class="inputs-control">\n                <ion-item>\n                    <ion-input type="text" name="coursecode" placeholder="{{ \'addon.evaluate.coursecode\' | translate }}" formControlName="coursecode" autocapitalize="none" autocorrect="off"></ion-input>\n                </ion-item>\n                <ion-item>\n                    <ion-input type="text" name="fullname" placeholder="{{ \'addon.evaluate.fullname\' | translate }}" formControlName="fullname" autocapitalize="none" autocorrect="off"></ion-input>\n                </ion-item>\n                <ion-item>\n                    <ion-input type="text" name="email" placeholder="{{ \'addon.evaluate.email\' | translate }}" formControlName="email" autocapitalize="none" autocorrect="off" ></ion-input>\n                </ion-item>\n                <ion-item margin-bottom>\n                    <ion-input type="text" name="cmnd" placeholder="{{ \'addon.evaluate.cmnd\' | translate }}" formControlName="cmnd" autocapitalize="none" autocorrect="off" ></ion-input>\n                </ion-item>\n                <button ion-button block [disabled]="!credForm.valid">{{ \'addon.evaluate.btnnext\' | translate }}</button> \n            </div>\n            <img class="thumbs-evaluate" src="assets/img/thumbs-evaluate.svg">\n        </form>\n    </div>\n</ion-content>\n'/*ion-inline-end:"/Users/vndtadmin/Desktop/Moodlemobile_lang/src/addon/evaluate/pages/input-info/input-info.html"*/,
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_3__providers_utils_dom__["a" /* CoreDomUtilsProvider */],
             __WEBPACK_IMPORTED_MODULE_2__core_courses_providers_courses__["a" /* CoreCoursesProvider */], __WEBPACK_IMPORTED_MODULE_4__providers_utils_utils__["a" /* CoreUtilsProvider */],
-            __WEBPACK_IMPORTED_MODULE_9__addon_block_timeline_providers_timeline__["a" /* AddonBlockTimelineProvider */], __WEBPACK_IMPORTED_MODULE_5__providers_sites__["a" /* CoreSitesProvider */],
-            __WEBPACK_IMPORTED_MODULE_10__core_course_providers_helper__["a" /* CoreCourseHelperProvider */], __WEBPACK_IMPORTED_MODULE_11__core_course_providers_options_delegate__["a" /* CoreCourseOptionsDelegate */],
-            __WEBPACK_IMPORTED_MODULE_6__providers_events__["a" /* CoreEventsProvider */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["t" /* NavController */], __WEBPACK_IMPORTED_MODULE_8__providers_app__["a" /* CoreAppProvider */],
-            __WEBPACK_IMPORTED_MODULE_14__providers_evaluate__["a" /* AddonEvaluateProvider */], __WEBPACK_IMPORTED_MODULE_13__angular_forms__["a" /* FormBuilder */], __WEBPACK_IMPORTED_MODULE_15__ngx_translate_core__["c" /* TranslateService */],
-            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["b" /* AlertController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["u" /* NavParams */], __WEBPACK_IMPORTED_MODULE_7__providers_data_service__["a" /* DataService */]])
+            __WEBPACK_IMPORTED_MODULE_10__addon_block_timeline_providers_timeline__["a" /* AddonBlockTimelineProvider */], __WEBPACK_IMPORTED_MODULE_5__providers_sites__["a" /* CoreSitesProvider */],
+            __WEBPACK_IMPORTED_MODULE_11__core_course_providers_helper__["a" /* CoreCourseHelperProvider */], __WEBPACK_IMPORTED_MODULE_12__core_course_providers_options_delegate__["a" /* CoreCourseOptionsDelegate */],
+            __WEBPACK_IMPORTED_MODULE_6__providers_events__["a" /* CoreEventsProvider */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["t" /* NavController */], __WEBPACK_IMPORTED_MODULE_9__providers_app__["a" /* CoreAppProvider */],
+            __WEBPACK_IMPORTED_MODULE_15__providers_evaluate__["a" /* AddonEvaluateProvider */], __WEBPACK_IMPORTED_MODULE_14__angular_forms__["a" /* FormBuilder */], __WEBPACK_IMPORTED_MODULE_16__ngx_translate_core__["c" /* TranslateService */],
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["b" /* AlertController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["u" /* NavParams */], __WEBPACK_IMPORTED_MODULE_8__providers_data_service__["a" /* DataService */],
+            __WEBPACK_IMPORTED_MODULE_7__core_user_providers_user__["a" /* CoreUserProvider */]])
     ], AddonEvaluateInputInfoPage);
     return AddonEvaluateInputInfoPage;
 }());
